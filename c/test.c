81c19479d5b2422aa2ab5e82122c48cdcc7bd238
kichanj <yongomn02@gmail.com>
2024-05-23 11:52:16 +0900

    test code add

---
+++ c/test.c
@@ -0,0 +1,9 @@
+//gcc -o test test.c
+
+#include<stdio.h>
+
+int main(){
+    printf("hello world c\n");
+
+    return 0;
+}
---
+++ cplus/test.cc
@@ -0,0 +1,9 @@
+// g++ -o test test.cc
+
+#include<iostream>
+
+int main()
+{
+    std::cout<< "hello world cplus"<<std::endl;
+    return 0;
+}
